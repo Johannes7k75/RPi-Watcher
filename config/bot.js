09a72ed2baddfc59b38b9af11bf08d357bb33094
Johannes7k75 <57235955+Johannes7k75@users.noreply.github.com>
2021-05-07 15:11:35 +0200

    Create bot.js

---
+++ config/bot.js
@@ -0,0 +1,8 @@
+module.exports = {
+    discord: {
+        token: 'YOR TOKEN HERE',
+        prefix: 'PREFIX',
+        activity: 'YOR ACTIVITY HERE',
+        rpilogschannel: 'THE CHANNEL WHERE HE LOGS WEHN HE GOES ONLINE',
+    },
+};
