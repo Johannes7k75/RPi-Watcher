e09a6555b0492a238e11e5e83b1a8fbf082835e9
Johannes7k75 <Jonashebenstreit1@gmail.com>
2021-05-08 20:01:33 +0200

    main

---
+++ commands/misc/ip.js
@@ -0,0 +1,22 @@
+module.exports = {
+    name: "ip",
+    aliases: ["i"],
+    category: 'Misc',
+    utilisation: '{prefix}ip',
+
+    async execute(client, message) {
+        const Discord = require("discord.js")
+        ifconfig = require('ifconfig.me');
+        ifconfig.get(function (data) {
+            const embdeIP = new Discord.MessageEmbed()
+                .setColor('#00ff00')
+                .setTitle('External IP')
+                .addFields(
+                    { name: 'IP ', value: data.ip_addr },
+                )
+            message.channel.send(embdeIP);
+        })
+    }
+}
+
+
