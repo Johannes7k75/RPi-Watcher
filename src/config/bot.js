98894fdb5d627a6f23d581d4e605855abbc2c74d
Johannes7k75 <Jonashebenstreit1@gmail.com>
2021-05-08 19:46:10 +0200

    main

--- src/config/bot.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    discord: {
-        token: '',
-        prefix: 'RPI!',
-        activity: 'https://player-lounge.de',
-        rpilogschannel: '817096752707403856',
-    },
-};
