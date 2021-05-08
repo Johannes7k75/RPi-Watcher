5feaf945e79de4dd1da9d2b3101bbd8ac4ea02a2
Johannes7k75 <57235955+Johannes7k75@users.noreply.github.com>
2021-05-08 16:49:26 +0200

    Delete bot.js

--- config/bot.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    discord: {
-        token: 'ODA4MDA3OTc5MjU2Nzc0NzM3.YCASEg.UH-VNZD_xqiLGVldW-gHOaRv78w',
-        prefix: 'RPI!',
-        activity: 'https://player-lounge.de',
-        rpilogschannel: '817096752707403856',
-    },
-};
