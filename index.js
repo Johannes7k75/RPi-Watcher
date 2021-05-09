ccefc5aac5376fcb0d07c398d443446c5c931a02
Johannes7k75 <Jonashebenstreit1@gmail.com>
2021-05-09 17:40:31 +0200

    main

--- index.js
+++ index.js
@@ -7,10 +7,17 @@ const date = new Date();
 
 client.commands = new Discord.Collection();
 client.config = require('./config/bot');
-const months = ["Januar", "Februar", "März", "April", "Mai", "Juni", "Juli", "August", "September", "Oktober", "November", "Dezember"];
+const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
 client.on('ready', () => {
-    client.channels.cache.get(client.config.discord.rpilogschannel).send('```' + 'Der Bot ist Online ' + date.getHours() + ':' + date.getMinutes() + ':' + date.getSeconds() + ' ' + date.getDate() + '. ' + months[date.getMonth()] + '```')
+    const embed = new Discord.MessageEmbed()
+        .setColor('RANDOM')
+        .setTitle('The Bot is online')
+        .addFields(
+            { name: 'Time ', value: `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`, inline: true },
+            { name: 'Date', value: `${date.getDate()}.${months[date.getMonth()]}.${date.getFullYear()}`, inline: true }
+        )
+    client.channels.cache.get(client.config.discord.rpilogschannel).send(embed)
 });
 
 ['command_handler', 'event_handler'].forEach(handler => {
