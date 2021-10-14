872f4a3129518b4e22b09a2d25809f21375847a6
Johannes7k75 <Jonashebenstreit1@gmail.com>
2021-10-14 18:47:48 +0200

    main

--- commands/misc/clear.js
+++ commands/misc/clear.js
@@ -4,14 +4,21 @@ module.exports = {
     category: 'moderation',
 
     async execute(client, message, args) {
-        if (!args[0]) return message.reply('please enter the amount of messages that you want to clear!');
+        if (!args[0])
+            return message.reply(
+                'please enter the amount of messages that you want to clear!'
+            );
         if (isNaN(args[0])) return message.reply('pleas type a real number!');
 
-        if (args[0] > 100) return message.reply("you can't delete more than 100 messages!");
-        if (args[0] < 1) return message.reply('you must delete atleast one message!');
+        if (args[0] > 100)
+            return message.reply("you can't delete more than 100 messages!");
+        if (args[0] < 1)
+            return message.reply('you must delete atleast one message!');
         await message.delete();
-        await message.channel.messages.fetch({ limit: Math.round(args[0]) }).then((messages) => {
-            message.channel.bulkDelete(messages);
-        });
+        await message.channel.messages
+            .fetch({ limit: Math.round(args[0]) })
+            .then(messages => {
+                message.channel.bulkDelete(messages);
+            });
     },
 };
--- commands/misc/ip.js
+++ commands/misc/ip.js
@@ -1,19 +1,23 @@
 module.exports = {
-	name: 'ip',
-	aliases: ['i'],
-	category: 'Misc',
-	utilisation: '{prefix}ip',
+    name: 'ip',
+    aliases: ['i'],
+    category: 'Misc',
+    utilisation: '{prefix}ip',
 
-	async execute(client, message) {
-		if (message.author.id == client.config.discord.bot_owner_id) {
-			const color = client.config.embed.color.ip;
-			ifconfig = require('ifconfig.me');
-			ifconfig.get(function (data) {
-				embed = { title: 'External IP', fields: [{ name: 'IP', value: `||${data.ip_addr}||` }], color: color }
-				message.channel.send({ embeds: [embed] });
-			});
-		} else {
-			message.channel.send('You are not the Owner');
-		}
-	},
+    async execute(client, message) {
+        if (message.author.id == client.config.discord.bot_owner_id) {
+            const color = client.config.embed.color.ip;
+            ifconfig = require('ifconfig.me');
+            ifconfig.get(function (data) {
+                embed = {
+                    title: 'External IP',
+                    fields: [{ name: 'IP', value: `||${data.ip_addr}||` }],
+                    color: color,
+                };
+                message.channel.send({ embeds: [embed] });
+            });
+        } else {
+            message.channel.send('You are not the Owner');
+        }
+    },
 };
