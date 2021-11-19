b195586a5f758bb1f58f506e48ba041081328b29
Johannes7k75 <57235955+Johannes7k75@users.noreply.github.com>
2021-11-19 20:10:26 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const {Client, Intents, Collection} = require('discord.js');
-const client = new Client({	intents:[Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES]});
+const client = new Client({ intents:[Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 
 client.commands = new Collection();
 client.config = require('./config/bot');
